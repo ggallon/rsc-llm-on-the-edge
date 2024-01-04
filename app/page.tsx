db6c488db55025ae75e8620863b3dae3179f9d6e
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-01-04 10:47:26 +0100

    Fix openai with the last version

--- app/page.tsx
+++ app/page.tsx
@@ -3,30 +3,27 @@ import { Footer } from "./components/footer";
 import { Region } from "./components/region";
 import { parseVercelId } from "./parse-vercel-id";
 import { OpenAIStream } from "ai";
-import { Configuration, OpenAIApi } from "openai-edge";
+import OpenAI from "openai";
 import { Suspense } from "react";
 import { Tokens } from "ai/react";
 import { kv } from "@vercel/kv";
 
 export const runtime = "edge";
 
-const apiConfig = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY!,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
 });
-
-const openai = new OpenAIApi(apiConfig);
 
 export default async function Page() {
   const headersList = headers();
   const city = decodeURIComponent(
-    headersList.get("X-Vercel-IP-City") || "San%20Francisco"
+    headersList.get("X-Vercel-IP-City") || "Bordeaux",
   );
 
-  const timezone =
-    headersList.get("X-Vercel-IP-Timezone") || "America/Los_Angeles";
+  const timezone = headersList.get("X-Vercel-IP-Timezone") || "Europe/Paris";
 
   const { proxyRegion, computeRegion } = parseVercelId(
-    headersList.get("X-Vercel-Id")!
+    headersList.get("X-Vercel-Id"),
   );
 
   if (headersList.get("user-agent")?.includes("Twitterbot")) {
@@ -63,16 +60,9 @@ export default async function Page() {
 
 // We add a wrapper component to avoid suspending the entire page while the OpenAI request is being made
 async function Wrapper({ city, timezone }: { city: string; timezone: string }) {
-  const prompt =
-    "Act like as if you are a travel expert. Provide a list of 5 things to do in " +
-    city +
-    " in the " +
-    // The timezone helps the AI decide the correct state / location
-    timezone +
-    " timezone and start with 'here's a...'. Do NOT mention the timezone in your response.";
-
+  const binome = `rllm:${city}-${timezone.replace("/", "-")}`;
   // See https://sdk.vercel.ai/docs/concepts/caching
-  const cached = (await kv.get(prompt)) as string | undefined;
+  const cached = (await kv.get(binome)) as string | undefined;
 
   if (cached) {
     const chunks = cached.split(" ");
@@ -85,8 +75,8 @@ async function Wrapper({ city, timezone }: { city: string; timezone: string }) {
             setTimeout(
               r,
               // get a random number between 10ms and 50ms to simulate a random delay
-              Math.floor(Math.random() * 40) + 10
-            )
+              Math.floor(Math.random() * 40) + 10,
+            ),
           );
         }
         controller.close();
@@ -96,7 +86,7 @@ async function Wrapper({ city, timezone }: { city: string; timezone: string }) {
     return <Tokens stream={stream} />;
   }
 
-  const response = await openai.createChatCompletion({
+  const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
     stream: true,
     messages: [
@@ -116,8 +106,8 @@ async function Wrapper({ city, timezone }: { city: string; timezone: string }) {
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response, {
     async onCompletion(completion) {
-      await kv.set(prompt, completion);
-      await kv.expire(prompt, 60 * 10);
+      await kv.set(binome, completion);
+      await kv.expire(binome, 60 * 10);
     },
   });
 
